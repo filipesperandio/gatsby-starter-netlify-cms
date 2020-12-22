44ffdc27b568acc18461b64b85d9b7f4a5246489
Filipe Esperandio <filipesperandio@gmail.com>
2020-12-22 20:43:17 -0300

    Fix index page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
+import React from "react"
+import { Link, graphql } from "gatsby"
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
-import PageImage from '../components/PageUnsplashImage'
+import Layout from "../components/Layout"
+import Features from "../components/Features"
+import BlogRoll from "../components/BlogRoll"
+import PageImage from "../components/PageUnsplashImage"
 
-const PRIMARY_COLOR = 'rgb(154, 1, 205)'
+const PRIMARY_COLOR = "rgb(154, 1, 205)"
 
 export const IndexPageTemplate = ({
   logo,
@@ -19,90 +19,69 @@ export const IndexPageTemplate = ({
   intro,
 }) => {
   return (
-  <div>
-    <PageImage src={image.childImageSharp.fluid.src} credit={imageCredit}>
-      <div
-        style={{
-          display: 'flex',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+    <div>
+      <PageImage src={image.childImageSharp.fluid.src} credit={imageCredit}>
+        <div
           style={{
-            boxShadow:
-              `${PRIMARY_COLOR} 0.5rem 0px 0px, ${PRIMARY_COLOR} -0.5rem 0px 0px`,
-            backgroundColor: PRIMARY_COLOR,
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              `${PRIMARY_COLOR} 0.5rem 0px 0px, ${PRIMARY_COLOR} -0.5rem 0px 0px`,
-            backgroundColor: PRIMARY_COLOR,
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-            textAlign: 'right',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </PageImage>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content wrapper">
-                  <div className="tile">
-                    <h3 className="subtitle center">{mainpitch.description}</h3>
+            display: "flex",
+            lineHeight: "1",
+            justifyContent: "space-around",
+            alignItems: "left",
+            flexDirection: "column",
+          }}>
+          <h1 className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen">
+            {title}
+          </h1>
+          <h3 className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen">
+            {subheading}
+          </h3>
+        </div>
+      </PageImage>
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="section">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <div className="content">
+                  <div className="content wrapper">
+                    <div className="tile">
+                      <h3 className="subtitle center">{mainpitch.description}</h3>
+                    </div>
+                    <div className="logo-container">
+                      <img className="image logo" src={logo.childImageSharp.fluid.src} />
+                    </div>
                   </div>
-                  <div className="logo-container">
-                    <img className="image logo" src={logo.childImageSharp.fluid.src} />
+                  <div className="columns">
+                    <div
+                      className="description column is-12"
+                      dangerouslySetInnerHTML={{ __html: description }}
+                      style={{
+                        lineHeight: "1.6",
+                        marginTop: "1rem",
+                      }}></div>
                   </div>
+                  <Features gridItems={intro.blurbs} />
+                  {false && (
+                    <>
+                      <div className="column is-12">
+                        <h3 className="has-text-weight-semibold is-size-2">Latest stories</h3>
+                        <BlogRoll />
+                        <div className="column is-12 has-text-centered">
+                          <Link className="btn" to="/blog">
+                            Read more
+                          </Link>
+                        </div>
+                      </div>
+                    </>
+                  )}
                 </div>
-                <div className="columns">
-                  <div className="description column is-12"
-                     dangerouslySetInnerHTML={{__html: description}} style={{
-                      lineHeight: '1.6',
-                      marginTop: '1rem',
-                    }}>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-    { false && (<>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-      </>)}
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </div>
-)
+      </section>
+    </div>
+  )
 }
 
 const IndexPage = ({ data }) => {
@@ -125,7 +104,6 @@ const IndexPage = ({ data }) => {
 }
 
 export default IndexPage
-
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
