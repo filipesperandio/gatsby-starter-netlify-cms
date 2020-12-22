7965c340152d45ac14d579b2d7b2be2986b24165
Filipe Esperandio <filipesperandio@gmail.com>
2020-12-22 20:41:44 -0300

    Adjust contact

---
+++ src/components/ContactForm.js
@@ -0,0 +1,102 @@
+import React from "react"
+
+import { navigate } from "gatsby-link"
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+const ContactForm = ({ children, formName, ...props }) => {
+  const [state, setState] = React.useState({ ...props })
+
+  const handleChange = (e) => {
+    setState({ [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/?no-cache=1", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch((error) => alert(error))
+  }
+
+  return (
+    <form
+      name="contact"
+      method="post"
+      action="/contact/thanks/"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit}>
+      {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+      <input type="hidden" name="form-name" value={props.formName || "contact"} />
+
+      <div hidden>
+        <label>
+          Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+        </label>
+      </div>
+      <div className="field">
+        <label className="label" htmlFor={"name"}>
+          Nome
+        </label>
+        <div className="control">
+          <input
+            className="input"
+            type={"text"}
+            name={"name"}
+            onChange={handleChange}
+            id={"name"}
+            required={true}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label" htmlFor={"email"}>
+          Email
+        </label>
+        <div className="control">
+          <input
+            className="input"
+            type={"email"}
+            name={"email"}
+            onChange={handleChange}
+            id={"email"}
+            required={true}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label" htmlFor={"message"}>
+          Mensagem
+        </label>
+        <div className="control">
+          <textarea
+            className="textarea"
+            name={"message"}
+            onChange={handleChange}
+            id={"message"}
+            required={true}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <button className="button" type="submit">
+          Enviar
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export { ContactForm }
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,115 +1,51 @@
 import React from "react"
-import { navigate } from "gatsby-link"
 import Layout from "../../components/Layout"
+import { Marker, Envelope } from "../../components/Icons"
+import { ContactForm } from "../../components/ContactForm"
 
-function encode(data) {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&")
-}
-
-export default class Index extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { isValidated: false }
-  }
-
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch("/?no-cache=1", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch((error) => alert(error))
-  }
-
-  render() {
-    return (
-      <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1>Contato</h1>
-              <p>Envie sua mensagem que daremos retorno em breve.</p>
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}>
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-
-                <div hidden>
-                  <label>
-                    Don’t fill this out: <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"name"}>
-                    Nome
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"text"}
-                      name={"name"}
-                      onChange={this.handleChange}
-                      id={"name"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"email"}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"email"}
-                      name={"email"}
-                      onChange={this.handleChange}
-                      id={"email"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"message"}>
-                    Mensagem
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={"message"}
-                      onChange={this.handleChange}
-                      id={"message"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <button className="button" type="submit">
-                    Enviar
-                  </button>
-                </div>
-              </form>
+const Index = (props) => {
+  return (
+    <Layout>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3603.213932512414!2d-49.26451510234615!3d-25.43111031731486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce46a637002d3%3A0xc7f133d0f842ba58!2sR.%20Conselheiro%20Laurindo%2C%20502%20-%20Centro%2C%20Curitiba%20-%20PR%2C%2080060-100!5e0!3m2!1sen!2sbr!4v1608640965855!5m2!1sen!2sbr"
+        width="100%"
+        height="400"
+        frameborder="0"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        ariaHidden="false"
+        tabindex="0"
+      />
+      <section className="section">
+        <div className="container">
+          <div className="content">
+            <div className="columns">
+              <div className="column icon">
+                <Marker className="mr-2" />
+              </div>
+              <div className="column">
+                Rua Conselheiro Laurindo, 502 - Sala 903
+                <br />
+                80060-100 - Curitiba/PR
+              </div>
             </div>
+            <div className="columns">
+              <div className="column icon">
+                <Envelope className="mr-2" />
+              </div>
+              <div className="column">
+                <a href="mailto:contato@espiritualidade-saude.org">
+                  contato@espiritualidade-saude.org
+                </a>
+              </div>
+            </div>
+            <h1>Fale conosco!</h1>
           </div>
-        </section>
-      </Layout>
-    )
-  }
+          <ContactForm />
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export default Index
