054e9d3a56fc87c82f52d9bb4d0247e50a6d3192
Filipe Esperandio <filipesperandio@gmail.com>
2020-12-25 22:48:17 -0300

    Fix attr spelling

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -14,11 +14,11 @@ const Index = ({ location }) => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3603.213932512414!2d-49.26451510234615!3d-25.43111031731486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce46a637002d3%3A0xc7f133d0f842ba58!2sR.%20Conselheiro%20Laurindo%2C%20502%20-%20Centro%2C%20Curitiba%20-%20PR%2C%2080060-100!5e0!3m2!1sen!2sbr!4v1608640965855!5m2!1sen!2sbr"
         width="100%"
         height="400"
-        frameborder="0"
+        frameBorder="0"
         style={{ border: 0 }}
-        allowfullscreen=""
-        ariaHidden="false"
-        tabindex="0"
+        allowFullScreen={false}
+        aria-hidden="false"
+        tabIndex="0"
       />
       <section className="section">
         <div className="container">
